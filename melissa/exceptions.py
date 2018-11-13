02aedd14c72fc4991808f5d870c4b4b94770c055
magnusknutas <magnus@thefarm.se>
2018-11-13 08:45:29 +0100

    Reworking the interface

--- melissa/exceptions.py
+++ melissa/exceptions.py
@@ -19,3 +19,20 @@ class ApiException(Exception):
     @property
     def status_code(self):
         return self._status_code
+
+
+class UnsupportedDevice(Exception):
+    def __init__(self, *args):
+        self._message = args[0]
+        try:
+            self._status_code = args[1]
+        except IndexError:
+            self._status_code = None
+
+    @property
+    def message(self):
+        return self._message
+
+    @property
+    def status_code(self):
+        return self._status_code
