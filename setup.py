8bca3ba9da30d1f2df929209bdfc2587162959fd
magnusknutas <magnus@thefarm.se>
2018-12-01 11:07:27 +0100

    2.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 __author__ = 'Magnus Knutas'
-VERSION = '2.1.2'
+VERSION = '2.1.3'
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
